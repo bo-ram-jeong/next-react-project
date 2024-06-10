e1fc8ec6b3af005f34aa323310868c25f38ab69d
boram <boram33377@gmail.com>
2024-06-10 20:22:15 +0900

    sns 애플리케이션 개발 완료 * socket.io 실시간 채팅 개발 * vanilla-extract 라이브러리 적용

--- sns-project/next.config.js
+++ sns-project/next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/upload/:slug',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:slug`, // Matched parameters can be used in the destination
+      },
+    ]
+  },
+}
+
+module.exports = withVanillaExtract(nextConfig);
